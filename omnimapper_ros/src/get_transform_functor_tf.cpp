6107d8e376a2594cebc47c86c605e69a9120f2f5
Shengye Wang <i@shengye.wang>
2019-11-18 11:10:26 -0800

    get_transform_functor_tf.cpp

--- omnimapper_ros/src/get_transform_functor_tf.cpp
+++ omnimapper_ros/src/get_transform_functor_tf.cpp
@@ -1,24 +1,29 @@
-#include <omnimapper_ros/get_transform_functor_tf.h>
-#include <omnimapper_ros/ros_time_utils.h>
+#include "omnimapper_ros/get_transform_functor_tf.h"
+
+#include "omnimapper_ros/ros_time_utils.h"
+#include "rclcpp/rclcpp.hpp"
 
 omnimapper::GetTransformFunctorTF::GetTransformFunctorTF(
-    std::string sensor_frame_name, std::string base_frame_name)
-    : tf_listener_(ros::Duration(120.0)),
+    const std::string& sensor_frame_name, const std::string& base_frame_name,
+    std::shared_ptr<rclcpp::Node> ros_node,
+    std::shared_ptr<tf2_ros::Buffer> tf_buffer)
+    : ros_node_(ros_node),
+      tf_buffer_(tf_buffer),
       sensor_frame_name_(sensor_frame_name),
       base_frame_name_(base_frame_name) {}
 
 Eigen::Affine3d omnimapper::GetTransformFunctorTF::operator()(
     omnimapper::Time t) {
-  ros::Time ros_time = ptime2rostime(t);
-  tf::StampedTransform tf_transform;
+  rclcpp::Time ros_time = ptime2rostime(t);
+  geometry_msgs::msg::TransformStamped tf_transform;
   try {
-    tf_listener_.waitForTransform(base_frame_name_, sensor_frame_name_,
-                                  ros_time, ros::Duration(0.2));
-    tf_listener_.lookupTransform(base_frame_name_, sensor_frame_name_, ros_time,
-                                 tf_transform);
-  } catch (tf::TransformException ex) {
+    tf_transform = tf_buffer_->lookupTransform(
+        base_frame_name_, sensor_frame_name_, tf2_ros::fromMsg(ros_time),
+        tf2::durationFromSec(0.2));
+  } catch (tf2::TransformException ex) {
     // printf ("GetTransformFunctorTF: Error looking up tf: %s\n", ex.what ());
-    ROS_ERROR("GetTransformFunctorTF: Error looking up tf: %s\n", ex.what());
+    RCLCPP_ERROR(ros_node_->get_logger(),
+                 "GetTransformFunctorTF: Error looking up tf: %s\n", ex.what());
     return (Eigen::Affine3d::Identity());
   }
 
